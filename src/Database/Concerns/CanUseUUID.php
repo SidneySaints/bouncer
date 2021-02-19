25e42ae5d67770ad58f880bc4abbf853b2468dda
Florian Beck-Klaus <f.beck-klaus@imi.de>
2021-02-19 14:34:56 +0100

    add trait for uuid

---
+++ src/Database/Concerns/CanUseUUID.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Silber\Bouncer\Database\Concerns;
+
+use Ramsey\Uuid\Uuid;
+
+trait CanUseUUID
+{
+    /**
+     * Indicates if the IDs are UUIDs.
+     *
+     * @return bool
+     */
+    protected function keyIsUuid(): bool
+    {
+        return config('bouncer.use_uuid');
+    }
+
+    /**
+     * The UUID version to use.
+     *
+     * @return int
+     */
+    protected function uuidVersion(): int
+    {
+        return config('bouncer.uuid_version', 4);
+    }
+
+    /**
+     * The "booting" method of the model.
+     */
+    public static function bootUuid(): void
+    {
+        static::creating(function (self $model): void {
+            // Automatically generate a UUID if using them, and not provided.
+            if ($model->keyIsUuid() && empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = $model->generateUuid();
+            }
+        });
+    }
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    protected function generateUuid(): string
+    {
+        switch ($this->uuidVersion()) {
+            case 1:
+                return RamseyUuid::uuid1()->toString();
+            case 4:
+                return RamseyUuid::uuid4()->toString();
+        }
+
+        throw new Exception("UUID version [{$this->uuidVersion()}] not supported.");
+    }
+}
